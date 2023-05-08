f3f20b5f760848e822136064052e63c83a393aa0
Alliestrasza <evllis.jolia@gmail.com>
2023-05-08 14:42:32 +0800

    修改生产请求域名

--- src/config/env.production.js
+++ src/config/env.production.js
@@ -1,8 +1,8 @@
 // 正式
 module.exports = {
     title: 'vue-h5',
-    baseUrl: 'https://www.techwis.cn/', // 正式项目地址
-    baseApi: 'https://www.techwis.cn/', // 正式api请求地址
+    baseUrl: 'https://www.myafq.com/', // 正式项目地址
+    baseApi: 'https://www.myafq.com/', // 正式api请求地址
     APPID: 'xxx',
     APPSECRET: 'xxx',
     $cdn: 'https://www.xxxx.cn/static'
